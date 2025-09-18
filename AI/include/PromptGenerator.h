73034ba5a55ee3da73624601a65bfdf3a0567228
Aggelos <agg.delis@gmail.com>
2025-09-18 20:14:42 +0300

    pareeeeee

--- AI/include/PromptGenerator.h
+++ AI/include/PromptGenerator.h
@@ -6,12 +6,14 @@ class PromptGenerator{
 public:
     enum class Prompt {EQ1, EQ2, SYS, PYTH};
 private:
+    Prompt problemType;
+
     std::string genEQ1();
     std::string genEQ2();
     std::string genSYS();
     std::string genPYTH();
 public:
-    PromptGenerator();
+    PromptGenerator(Prompt);
 
     std::string generate();
 
