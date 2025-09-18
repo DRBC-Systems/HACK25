dcbbcc731f810d984927639b6d132929ec853800
Aggelos <agg.delis@gmail.com>
2025-09-18 20:45:39 +0300

    header sol-function added

--- AI/include/PromptGenerator.h
+++ AI/include/PromptGenerator.h
@@ -9,12 +9,19 @@ private:
     Prompt problemType;
 
     std::string genEQ1();
+    int solEQ1();
+
     std::string genEQ2();
+    int solEQ2();
+
     std::string genSYS();
+    int solSYS();
+    
     std::string genPYTH();
+    int solPYTH();
 public:
     PromptGenerator(Prompt);
-
+    int sol(Prompt);
     std::string generate();
 
 };
